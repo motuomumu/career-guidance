995442889b1d768db9999fb400b7fa26437e35dd
jiajiqi <3160249813@qq.com>
2019-12-26 15:03:01 +0800

    revise

---
+++ bin/src/main/resources/static/precache-manifest.6ab0dfdbf26cca74323b3192dc006d2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42536545430106a898860c7022691d80",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b9ebb37c403b42456557",
+    "url": "./static/css/2.ae83a650.chunk.css"
+  },
+  {
+    "revision": "d27c0c403c66dbe2f0e8",
+    "url": "./static/css/main.a5e7314a.chunk.css"
+  },
+  {
+    "revision": "b9ebb37c403b42456557",
+    "url": "./static/js/2.74d84630.chunk.js"
+  },
+  {
+    "revision": "d27c0c403c66dbe2f0e8",
+    "url": "./static/js/main.76f0c5b0.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
